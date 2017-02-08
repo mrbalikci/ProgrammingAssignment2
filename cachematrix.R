606df996c578f891358dbfd6e0b6200ea6b22340
mrbalikci <mrbalikci@gmail.com>
2017-02-08 03:58:08 -0600

    Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Erdem John Balikci, February 8, 2017
 
-## Write a short comment describing this function
+## Programming Assignment 2: Lexical Scoping
+
+## Matrix inversion by pair of functions 
+## (makeCacheMatrix and cacheSolve) that 
+## cache the invers of a matrix (invertible square)
+
+## makeCacheMatrix is a function which creates a 
+## special "matrix" object that can 
+## cache its inverse for the input 
+## (which is an invertible square matrix)
+
+## set the value of matrix
+## get the value of matrix
+## set the inverse of matrix
+## get the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
+## cacheSolve is a function which computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed) then the cacheSolve should retrieve the 
+## inverse from the cache. 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting the cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
+
+## Example / Usage: 
+
+## > m <- matrix(c(7,0,-3,2,3,4,1,-1,-2), nrow=3, ncol=3)
+##> m
+## [,1] [,2] [,3]
+## [1,]    7    2    1
+## [2,]    0    3   -1
+## [3,]   -3    4   -2
+## > MyMatrix <- makeCacheMatrix(m)
+## > cacheSolve(MyMatrix)
+## [,1] [,2] [,3]
+## [1,]   -2    8   -5
+## [2,]    3  -11    7
+## [3,]    9  -34   21
